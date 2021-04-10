b68bb9ed5772dea8cac54d32f2f47e28b1d888bd
prdm0 <pedro.rafael.marinho@gmail.com>
2021-04-10 12:45:11 -0300

    Update color.

--- channel_averages.R
+++ channel_averages.R
@@ -1,19 +1,30 @@
 library(magick)
 
-channel_averages <- function(path_img, alpha = 0.4, ...) {
-  img <- as.integer(image_data(image_read(path = path_img), ...))
+channel_averages <- function(path_img, alpha = 0.4, probs = 0.75, ...) {
   
-  vec_rgb <- "names<-"(vapply(
-    X = 1L:3L,
-    FUN = function(i)
-      median(img[, , i]) / 255,
-    FUN.VALUE = double(length = 1L)
-  ),
-  c("r", "g", "b"))
+  is_hex <- grepl("^#(\\d|[a-f]){6,8}$", path_img, ignore.case = TRUE)
   
-  rgb(red = vec_rgb["r"], green = vec_rgb["g"], blue = vec_rgb["b"], alpha = alpha)
-
+  if(is_hex) path_img
+  else {
+    img <- as.integer(image_data(image_read(path = path_img)))
+    
+    vec_rgb <- "names<-"(vapply(
+      X = 1L:3L,
+      FUN = function(i)
+        quantile(img[, , i], probs = probs, ...) / 255,
+      FUN.VALUE = double(length = 1L)
+    ),
+    c("r", "g", "b"))
+    
+    rgb(red = vec_rgb["r"], green = vec_rgb["g"], blue = vec_rgb["b"], alpha = alpha)
+    
+  }
+  
 }
 
-color <- channel_averages(path_img = "~/Downloads/VibrantColorR/logo.png")
-hist(rnorm(100), col = color)
+# .book .book-summary ul.summary li a:hover, .book .book-summary ul.summary li.active > a { color: #000000; background: 0 0; text-decoration: none; background-color: #FFAF24; }
+#     
+# .book .book-summary ul.summary li a:hover { background-color: #aaaaaa; color: #000000; }
+      
+color <- channel_averages(path_img = "/home/prdm0/Downloads/VibrantColorR/capa.png")
+pie(1, col = color, labels = NA)
